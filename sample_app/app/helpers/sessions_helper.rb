17c31b58c63f533e413f62c2d306e91349a68f39
benjdj6 <benjdj6@vt.edu>
2017-03-01 22:21:11 -0500

    Nav bar now changes appropriately upon login

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -2,12 +2,17 @@ module SessionsHelper
 
   # logs in specified user
   def log_in(user)
-    session[:user_id] = user.user_id
+    session[:user_id] = user.id
   end
 
   # returns the current logged-in user, if any
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the user is logged in, false if not
+  def logged_in?
+    !current_user.nil?
   end
 
 end
