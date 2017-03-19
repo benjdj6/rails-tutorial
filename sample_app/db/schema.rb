e17e42e9329ca6d2cc07f63cbbd557bb6748e620
benjdj6 <benjdj6@vt.edu>
2017-03-18 22:52:59 -0400

    Added sample follow sata

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319023250) do
+ActiveRecord::Schema.define(version: 20170319025144) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
